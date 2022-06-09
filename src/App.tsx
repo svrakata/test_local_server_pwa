623bbb794b526865a2b2ef5d6257e240254adbe7
svrakata <svrakata@gmail.com>
2022-06-09 08:51:10 +0300

    up sw

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [result, setResult] = useState<string | null>(null);
+    const [loading, setLoading] = useState(false);
+    const load = async () => {
+        setLoading(true);
+        try {
+            const res = await fetch("localhost:3000");
+            const payload = await res.text();
+            setResult(payload);
+            setLoading(false);
+        } catch (error) {
+            setResult("error");
+            setLoading(false);
+        }
+    };
+    useEffect(() => {
+        load();
+    }, []);
+
+    if (loading) {
+        return <div> ...loading</div>;
+    }
+
+    return (
+        <div>
+            {result !== null && <div style={{ fontSize: "48px", fontWeight: "bold" }}>{result}</div>}
+            <button onClick={() => load()}>refetch</button>
+        </div>
+    );
+};
 
 export default App;
